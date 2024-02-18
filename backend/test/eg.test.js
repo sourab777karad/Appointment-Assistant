0239cee110703f46fa1f6280e67feda8d92fd8cc
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2024-02-18 14:04:14 +0530

    modified backend test

--- backend/test/eg.test.js
+++ backend/test/eg.test.js
@@ -3,15 +3,6 @@ test("firebase-admin is installed", () => {
 	try {
 		require("firebase-admin");
 	} catch (error) {
-		throw new Error("Mongoose is not installed");
-	}
-});
-
-// Test if mongodb is installed
-test("mongodb is installed", () => {
-	try {
-		require("mongodb");
-	} catch (error) {
-		throw new Error("Firebase is not installed");
+		throw new Error("firebase is not installed");
 	}
 });
