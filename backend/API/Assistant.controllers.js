eccb4c519c0b2ff1d0848bce904a0bc7c9b28b5b
sourab777karad <1032211150@mitwpu.edu.in>
2024-02-18 23:13:35 +0530

    added Delete_appointment, change_status, get_profile, get_profile functionalities

--- backend/API/Assistant.controllers.js
+++ backend/API/Assistant.controllers.js
@@ -95,5 +95,51 @@ static async login(req, res) {
             return res.status(500).json({ message: 'Error setting appointment' });
         }
     }
+    static async  getUsers(req, res) {
+        try {
+            const users = await AssistantDAO.getUsers();
+            res.status(200).json(users);
+        } catch (e) {
+            console.error(`Unable to get users: ${e}`);
+            res.status(500).json({ message: `Unable to get users: ${e}` });
+        }
+    }
+    static async getProfile(req, res) {
+        try {
+          // Assuming you have some authentication middleware that verifies the token
+          // and attaches user information to the request object, you can use it to get the user ID.
+    
+          const userId = req.user.id; // Replace with the actual property containing user ID
+    
+          // If you have the user ID, proceed to get profile stats
+          const statistics = await AssistantDAO.getProfile(userId);
+    
+          return res.status(200).json({ data: statistics, message: 'Profile statistics retrieved successfully' });
+        } catch (err) {
+          console.error(err);
+          return res.status(500).json({ message: 'Error retrieving profile statistics' });
+        }
+      }
+      static async deleteAppointment(req, res) {
+        try {
+            const appointmentId = req.body.appointmentId; // Get the appointment ID from the request parameters
+            const result = await AssistantDAO.deleteAppointment(appointmentId);
+            res.status(200).json(result);
+        } catch (e) {
+            console.error(`Unable to delete appointment: ${e}`);
+            res.status(500).json({ message: `Unable to delete appointment: ${e}` });
+        }
+    }
+    static async changeStatus(req, res) {
+        try {
+            const appointmentId = req.body.appointment_id; // Get the appointment ID from the request body
+            const status = req.body.status; // Get the status from the request body
+            const result = await AssistantDAO.changeStatus(appointmentId, status);
+            res.status(200).json(result);
+        } catch (e) {
+            console.error(`Unable to change status: ${e}`);
+            res.status(500).json({ message: `Unable to change status: ${e}` });
+        }
+    }
     
 }
