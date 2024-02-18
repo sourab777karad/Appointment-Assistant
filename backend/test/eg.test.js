c188ea947dad5eafb9d136a00e27ae4ef591ae88
Parth Zarekar <90327206+Parth4123@users.noreply.github.com>
2024-02-18 15:36:07 +0530

    not using mongodb test case

--- backend/test/eg.test.js
+++ backend/test/eg.test.js
@@ -6,12 +6,3 @@ test("firebase-admin is installed", () => {
 		throw new Error("firebase is not installed");
 	}
 });
-
-// Test mongodb connection to atlas using mongodb
-test("mongodb is installed", () => {
-	try {
-		require("mongodb");
-	} catch (error) {
-		throw new Error("mongodb is not installed");
-	}
-});
