e287521cf6a408c7f39ee594f3eaeda8193e186c
Sourab karad <119159021+sourab777karad@users.noreply.github.com>
2024-03-21 10:19:20 +0530

    Update BaseUrlContext.jsx

--- frontend/website/src/context/BaseUrlContext.jsx
+++ frontend/website/src/context/BaseUrlContext.jsx
@@ -6,8 +6,8 @@ export const BaseUrlContext = createContext();
 export const BaseUrlProvider = ({ children }) => {
 	const [baseUrl, setBaseUrl] = useState(
 		// "https://brightly-vital-panther.ngrok-free.app",
-		"http://localhost:3000/assistant"
-		// "https://h1gk4w07-3000.euw.devtunnels.ms/assistant"
+		// "http://localhost:3000/assistant"
+		"https://h1gk4w07-3000.euw.devtunnels.ms/assistant"
 	);
 
 	return (
